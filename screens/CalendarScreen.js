68d8b83b46106076bf2c49d141c0914090819c8c
yiokchu <79416436+yiokchu@users.noreply.github.com>
2022-07-13 13:15:56 -0400

    Update Calendar Screen

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -2,70 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
-/*
-<View style={styles.container}>
-      
-    <Calendar
-        style={[styles.calendar, {width: 400, height: 200}]}
-        dayComponent={({date, state}) => {
-            return (
-            <View>
-                <Text style={{textAlign: 'center', color: state === 'disabled' ? 'gray' : 'black'}}>{date.day}</Text>
-            </View>
-            );
-        }}
-        /> 
-    </View>
-*/
-
-
 export default function CalendarScreen({navigation}){
   return (
     <CalendarList
-        // Max amount of months allowed to scroll to the past. Default = 50
+        // Max amount of months allowed to scroll to the past
         pastScrollRange={50}
-        // Max amount of months allowed to scroll to the future. Default = 50
+        // Max amount of months allowed to scroll to the future
         futureScrollRange={50}
         // Enable or disable scrolling of calendar list
         scrollEnabled={true}
-        // Enable or disable vertical scroll indicator. Default = false
+        // Enable or disable vertical scroll indicator
         showScrollIndicator={true}
     />
-
-    /*
-    <View style={styles.container}>
-        <Calendar
-          style={{ 
-            height: "100%",
-          }}
-          dayComponent={(e) => {
-            return (
-              <View style={{ width: 40, height: 40, backgroundColor:"orange"}}>
-                <Text> {e.date.day}</Text>
-              </View>
-            )
-          }}
-          renderHeader = {(date) => {
-            return (
-              <Text style={{fontWeight: 'bold', color: 'black'}}> 
-                {date.getMonth()}
-              </Text>
-            )
-          }}
-          theme = {
-            {
-              arrowColor: 'black',
-            }
-          }
-          hideExtraDays
-          markingType={'custom'}
-          markedDates={{
-            '2022-07-28': {customStyles:{container:{backgroundColor:'black', elevation:2}, text:{color:'white'}}}
-          }}
-        />
-    </View>*/
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
